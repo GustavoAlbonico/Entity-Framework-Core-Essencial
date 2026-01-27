e84a1d3ed262e2ffe822d4a5f515c05a3f677871
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2026-01-27 20:52:16 -0300

    feat - adicionando FuncionarioDetalhe

---
+++ GaSoft/GaSoft.Domain/Entities/FuncionarioDetalhe.cs
@@ -0,0 +1,17 @@
+﻿namespace GaSoft.Domain.Entities;
+
+public class FuncionarioDetalhe
+{
+    public int Id { get; set; }
+    public string? EnderecoResidencial { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string? Celular {  get; set; }
+    public string? Genero { get; set; }
+    public string? Foto { get; set; }
+    public string? EstadoCivil { get; set; }
+    public string? CPF { get; set; }
+    public string? Nacionalidade { get; set; }
+    public string? Escolaridade { get; set; }
+    public int FuncionarioId { get; set; }
+    public Funcionario? Funcionario { get; set; }
+}
